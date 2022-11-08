c3b3fc70dce31ec1d72ebee3b686f3a4ef790dbc
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2022-11-08 18:17:56 -0300

    General settings

--- index.js
+++ index.js
@@ -2,24 +2,27 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const conn = require("./config/db");
-const User = require("./models/UserModel");
+const conn = require("./src/config/db");
+const User = require("./src/models/UserModel");
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-  res.header("Access-Control-Allow-Origin", "*");
-  //Quais são os métodos que a conexão pode realizar na API
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,"
+  );
   app.use(cors());
   next();
 });
 
 // Import Routes
-const UserRouter = require("../src/routes/UserRouter");
+const UserRouter = require("./src/routes/UserRouter");
 
 // Routes
 app.use(UserRouter);
